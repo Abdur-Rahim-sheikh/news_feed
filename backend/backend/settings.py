a1354d681b63806f3b26bf421d8fd9ad7b342e68
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-23 19:49:42 +0600

    attaching postgress as default

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from environs import Env
+
+env = Env()
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,9 +78,14 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "POSTGRES_USER": env.str("POSTGRES_USER"),
+        "POSTGRES_PASSWORD": env.str("POSTGRES_PASSWORD"),
     }
 }
 
