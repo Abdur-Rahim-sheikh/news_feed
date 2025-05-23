4122f96e3fd97cec40e40ba1d5f39538ff670dfd
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-23 19:29:03 +0600

    custom user built

--- backend/newsroom/models.py
+++ backend/newsroom/models.py
@@ -1,13 +1,21 @@
-import pycountry
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 
-default_language = str
+# Create your models here.
+def default_countries():
+    return ["nz", "us"]
+
+
+def default_sources():
+    return ["bbc-news", "cnn"]
+
+
+def default_keywords():
+    return ["car", "automobile"]
 
 
 class User(AbstractUser):
-    countries = models.JSONField(default=["nz", "us"])
-    sources = models.JSONField(default=["bbc-news", "cnn"])
-    keywords = models.JSONField(default=["car", "automobile"])
+    countries = models.JSONField(default=default_countries)
+    sources = models.JSONField(default=default_sources)
+    keywords = models.JSONField(default=default_keywords)
