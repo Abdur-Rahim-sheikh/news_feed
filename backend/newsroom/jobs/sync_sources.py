35da74557df885f56493159eb63b261bee86da83
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-24 20:31:35 +0600

    increase id name

--- backend/newsroom/jobs/sync_sources.py
+++ backend/newsroom/jobs/sync_sources.py
@@ -1,10 +1,13 @@
+import logging
 from datetime import datetime
 
 from environs import Env
 from newsapi import NewsApiClient
 
 from newsroom.models import Source as sourcedb
-from newsroom.schemas import Source
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 env = Env()
 env.read_env()
@@ -15,8 +18,27 @@ def sync_sources():
     # scheduler running twice at each interval
     # handle this later seperating the schedule call
     # from original call 'runserver'
-    print(f"{datetime.now().date()=} running simple job", flush=True)
     db = sourcedb.objects.all().order_by("id")
-    sources = news_api.get_sources()
+    response = news_api.get_sources()
+    sources = {
+        src["id"]: sourcedb(
+            id=src["id"],
+            name=src["name"],
+            url=src["url"],
+            language_code=src["language"],
+            country_code=src["country"],
+        )
+        for src in response["sources"]
+        if src["id"]
+    }
+    exclude = []
+    for row in db:
+        if row.id in sources:
+            sources.pop(row.id)
+        else:
+            exclude.append(row.id)
 
-    print(db, flush=True)
+    include = sources.values()
+    sourcedb.objects.filter(pk__in=exclude).delete()
+    sourcedb.objects.bulk_create(include)
+    logger.info(f"new {exclude=}, {include=}")
--- backend/newsroom/models.py
+++ backend/newsroom/models.py
@@ -24,16 +24,16 @@ class User(AbstractUser):
 class News(models.Model):
     source_url = models.URLField(unique=True)
     country_code = models.CharField(max_length=3)
-    source_id = models.CharField(max_length=20)
+    source_id = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
-    source_name = models.CharField(max_length=100, blank=True, null=True)
+    source_name = models.CharField(max_length=200, blank=True, null=True)
     summary = models.CharField(max_length=5000)
     publication_date = models.DateTimeField(null=True, blank=True)
     thumbnail = models.URLField(blank=True, null=True, default=None)
 
 
 class Source(models.Model):
-    id = models.CharField(primary_key=True, max_length=20)
+    id = models.CharField(primary_key=True, max_length=100)
     name = models.CharField(max_length=200, blank=True, null=True)
     url = models.URLField(blank=True, null=True)
     language_code = models.CharField(max_length=3, blank=True, null=True)
