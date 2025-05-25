9e1f34a5b3482001a102662a8ca9130a921e2629
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-25 20:11:57 +0600

    demo navbar

---
+++ frontend/components/layout/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="bg-gray-800">
+        <div class="mx-auto px-5">
+            <header>
+                <div class="relative flex h-16 items-center justify-between">
+                    <h2 class="text-white text-4xl">Newsroom</h2>
+                    <img src="public/icons/profile.svg" class="w-8 h-8" alt="profile icon">
+                </div>
+            </header>
+        </div>
+    </nav>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,2 @@
 <template>
-    <h1 class="text-3xl font-bold underline"> Hello tailwind</h1>
 </template>
