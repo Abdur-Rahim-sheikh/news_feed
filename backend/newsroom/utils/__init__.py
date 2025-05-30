fdd484ee27034bf42a5ce6c7f019f814e9d8eae4
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-30 10:30:10 +0600

    jwt decorator

--- backend/newsroom/utils/__init__.py
+++ backend/newsroom/utils/__init__.py
@@ -1,3 +1,4 @@
+from .auth_decorator import jwt_required
 from .user_form import UserForm
 
-__all__ = ["UserForm"]
+__all__ = ["UserForm", "jwt_required"]
---
+++ backend/newsroom/utils/auth_decorator.py
@@ -0,0 +1,33 @@
+from functools import wraps
+
+import jwt
+from django.http import JsonResponse
+from environs import Env
+
+from ..models import User
+
+env = Env()
+env.read_env()
+
+
+def jwt_required(view_func):
+    @wraps(view_func)
+    def _wrap_view(request, *args, **kwargs):
+        auth_header = request.headers.get("Authorization")
+
+        if not auth_header or not auth_header.startswith("Bearer "):
+            return JsonResponse({"error": "Unauthorized Bearer"}, status=401)
+
+        token = auth_header.split(" ")[-1]
+
+        try:
+            payload = jwt.decode(
+                token, env.str("JWT_SECRET"), algorithms=[env.str("JWT_ALGORITHM")]
+            )
+            request.user_id = payload["id"]
+        except (jwt.ExpiredSignatureError, jwt.DecodeError, User.DoesNotExist) as e:
+            return JsonResponse({"error": f"Unauthorized decode {e}"}, status=401)
+
+        return view_func(request, *args, **kwargs)
+
+    return _wrap_view
