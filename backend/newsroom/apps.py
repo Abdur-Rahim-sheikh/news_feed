ae2a5aa220006da44aeeb6e6b3ec146c42ea4da1
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-31 17:43:06 +0600

    chore: scheduler

--- backend/newsroom/apps.py
+++ backend/newsroom/apps.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.apps import AppConfig
@@ -16,9 +16,12 @@ class NewsroomConfig(AppConfig):
         if not scheduler.running:
             # run once just at startup
             app_start = datetime.now()
-            # scheduler.add_job(sync_sources, "date", run_date=app_start)
-            # scheduler.add_job(
-            #     sync_news, "interval", minutes=10, next_run_time=app_start
-            # )
+            scheduler.add_job(sync_sources, "date", run_date=app_start)
+            scheduler.add_job(
+                sync_news,
+                "interval",
+                minutes=10,
+                next_run_time=app_start + timedelta(seconds=2),
+            )
 
-            # scheduler.start()
+            scheduler.start()
