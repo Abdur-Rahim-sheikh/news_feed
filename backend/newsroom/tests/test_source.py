bbcb2adbc8495683fca838cf516fee9eda6fe1a5
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-24 20:01:17 +0600

    settting up test

---
+++ backend/newsroom/tests/test_source.py
@@ -0,0 +1,13 @@
+from ..models import Source
+from django.test import TestCase
+
+
+class SourceModelTest(TestCase):
+    def setUp(self):
+        Source.objects.create(
+            name="uv", url="www.example.com", language_code="py", country_code="un"
+        )
+
+    def test_object_created(self):
+        data = Source.objects.all()
+        self.assertEqual(len(data), 1)
