2987fbc7846fa73e409cb307d873212980b140e4
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-25 02:11:20 +0600

    full_name test

--- backend/newsroom/tests/test_user.py
+++ backend/newsroom/tests/test_user.py
@@ -1,10 +1,30 @@
 from django.test import TestCase
-from newsroom.models import User
+from newsroom.models import User as userdb
+from ..schemas import User
 
 
 class TestUser(TestCase):
     def setUp(self):
-        self.user = User.objects.create(username="abir", email="admin@admin.com")
+        self.user = userdb.objects.create(
+            username="abir",
+            email="admin@admin.com",
+            first_name="abir",
+            last_name="sheikh",
+        )
 
-    def test_get_full_name(self):
+    def test_username(self):
         self.assertEqual(self.user.username, "abir")
+
+    def test_full_name(self):
+        user = User(
+            username=self.user.username,
+            email=self.user.email,
+            first_name=self.user.first_name,
+            last_name=self.user.last_name,
+            country_codes=self.user.country_codes,
+            source_ids=self.user.source_ids,
+            keywords=self.user.keywords,
+            password=self.user.password,
+            id=self.user.id,
+        )
+        self.assertEqual(user.full_name, "abir sheikh")
