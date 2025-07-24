ab00cbfe3a87333f3ff793a8c625c64579bcb4e6
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-24 20:28:42 +0600

    silencing the scheduler

--- backend/newsroom/apps.py
+++ backend/newsroom/apps.py
@@ -13,15 +13,16 @@ class NewsroomConfig(AppConfig):
     def ready(self):
         from .jobs import sync_news, sync_sources
 
-        if not scheduler.running:
-            # run once just at startup
-            app_start = datetime.now()
-            scheduler.add_job(sync_sources, "date", run_date=app_start)
-            scheduler.add_job(
-                sync_news,
-                "interval",
-                minutes=10,
-                next_run_time=app_start + timedelta(seconds=2),
-            )
+        # silencing the scheduler
+        # if not scheduler.running:
+        #     # run once just at startup
+        #     app_start = datetime.now()
+        #     scheduler.add_job(sync_sources, "date", run_date=app_start)
+        #     scheduler.add_job(
+        #         sync_news,
+        #         "interval",
+        #         minutes=10,
+        #         next_run_time=app_start + timedelta(seconds=2),
+        #     )
 
-            scheduler.start()
+        #     scheduler.start()
